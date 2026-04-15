4c428564777996ae76a68a7efedb4f53fda93cb3
Ivan Seremet <ivanseremet22@gmail.co>
2026-04-15 13:23:05 +0300

    test deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-    base: "./",
-    plugins: [react()],
-    server: {
-        host: true,
-    },
-    preview: {
-        host: true,
-    },
-});
+  plugins: [react()],
+  base: '/',
+})
